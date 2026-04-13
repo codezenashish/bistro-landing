26f71189ede621693c3f11e8c827809618a1831d
codezenashish <ixtashish@gmail.com>
2026-04-13 23:07:23 +0530

    feat: add multer middleware for file uploads

---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,16 @@
+import multer from "multer";
+const TEMP_UPLOAD_DIRECTORY: string = "./public/temp";
+
+const diskStorageConfiguration = multer.diskStorage({
+  destination: function setUploadDestination(request, file, callback) {
+    callback(null, TEMP_UPLOAD_DIRECTORY);
+  },
+
+  filename: function setUploadedFileName(request, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+export const fileUploadMiddleware = multer({
+  storage: diskStorageConfiguration,
+});
