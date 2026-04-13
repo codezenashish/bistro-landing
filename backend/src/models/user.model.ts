29c5c8fab94e9a4206e44b9cc88c52850bca014a
codezenashish <ixtashish@gmail.com>
2026-04-13 23:07:29 +0530

    feat: implement user model with authentication methods and password handling

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,102 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// 1. Interface define karein taaki TS ko methods ka pata chale
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  phoneNumber: string;
+  fullName: string;
+  avatar: string;
+  password: string;
+  refreshToken?: string | null;
+  orderHistory: mongoose.Types.ObjectId[];
+  isPasswordValid(plainTextPass: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is needed for delivery"],
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+    orderHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Password hashing
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Password validation
+userSchema.methods.isPasswordValid = async function (plainTextPass: string) {
+  return await bcrypt.compare(plainTextPass, this.password);
+};
+
+// Access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY as any }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY as any }
+  );
+};
+
+export const User = mongoose.model<IUser>("User", userSchema);
