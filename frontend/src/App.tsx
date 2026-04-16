d089bb9cca6a54040650ab194698f307f6a09a7b
codezenashish <ixtashish@gmail.com>
2026-04-16 23:21:25 +0530

    feat: restructure App component to include routing and toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "react-hot-toast"; 
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
