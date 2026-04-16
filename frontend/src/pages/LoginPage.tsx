0e51127e18922d09748f55d6cfcbe096d643c53e
codezenashish <ixtashish@gmail.com>
2026-04-16 23:23:04 +0530

    feat: add LoginPage component to handle user login

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import Login from "../features/auth/components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="page-wrapper">
+       <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
